8c615093daa20ee76c839371473243783d75d040
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-10-20 08:32:56 -0700

    output to file works

--- app/src/main/java/org/GraphGenerator/FileManager.java
+++ app/src/main/java/org/GraphGenerator/FileManager.java
@@ -7,17 +7,26 @@ import java.util.ArrayList;
 
 public class FileManager {
     public static String fileName = "output.txt";
+    public static int maxCol = 20;
 
     public static void outputToFile(ArrayList<Vertex> vertices, ArrayList<Line> lines) {
         File fd = new File("graphs/" + fileName);
+        int colCount = 1;
 
         try(PrintWriter pw = new PrintWriter(fd)) {
             for(Vertex v : vertices) {
-                pw.write("\\" + v.toString());
+                if(colCount >= maxCol) {
+                    pw.write("\n");
+                    colCount = 0;
+                }
+                pw.write("\\" + v.x + "," + v.y);
+                colCount++;
             }
             pw.write("\n");
             for(Line l : lines) {
-                pw.write("\\" + l.toString());
+                pw.write("\\(" + l.startingPoint.x + "," + l.startingPoint.y + "),(" 
+                    + l.endingPoint.x + "," + l.endingPoint.y + ")\n");
+                colCount++;
             }
         } catch(Exception ex) {
             System.out.println("Error in saving");
